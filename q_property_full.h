21c5e867a7df257726877559b9439239f91b98b8
karl-heinz reichel <kh@reichel-langer.de>
2016-08-14 15:51:06 +0200

    Added property macros for values, objects and read only values

--- q_property_full.h
+++ q_property_full.h
@@ -1,9 +1,30 @@
-#define Q_PROPERTY_FULL(member, _type) \
+#define Q_PROPERTY_FULL_VAL(member, _type) \
   Q_PROPERTY(_type member READ member WRITE set##member NOTIFY member##Changed) \
   public: \
-    void set##member##(_type _arg_##member) { if (m_##member != _arg_##member) {m_##member = _arg_##member; Q_EMIT member##Changed();  } } \
-    _type member##() const { return m_##member; } \
+    void set##member(_type _arg_##member) { if (m_##member != _arg_##member) {m_##member = _arg_##member; Q_EMIT member##Changed();  } } \
+    _type member() const { return m_##member; } \
   Q_SIGNALS: \
     void member##Changed(void); \
   private: \
     _type m_##member;
+
+#define Q_PROPERTY_FULL_REF(member, _type) \
+  Q_PROPERTY(_type member READ member WRITE set##member NOTIFY member##Changed) \
+  public: \
+    void set##member(const _type& _arg_##member) { if (m_##member != _arg_##member) {m_##member = _arg_##member; Q_EMIT member##Changed();  } } \
+    _type member() const { return m_##member; } \
+  Q_SIGNALS: \
+    void member##Changed(void); \
+  private: \
+    _type m_##member;
+
+
+#define Q_PROPERTY_READONLY(member, _type) \
+  Q_PROPERTY(_type member READ member NOTIFY member##Changed) \
+  public: \
+    _type member() const { return m_##member; } \
+  Q_SIGNALS: \
+    void member##Changed(void); \
+  private: \
+    _type m_##member;
+
